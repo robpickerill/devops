08e8f8cf8aebe6f0a51688a751ad2c0be11b3312
ayogi <yogeshiiith@gmail.com>
2016-06-23 08:06:10 +0530

    use logging

--- linux/monitoring/monitor-disk-io.sh
+++ linux/monitoring/monitor-disk-io.sh
@@ -6,13 +6,21 @@ out=$(df -BG /dev/sda5 | awk '{print $3}' | grep -iv used)
 
 out=${out/G}
 
-echo "Disk usage: $out"
+date >> $logfile
+echo "Disk usage: $out" >> $logfile
 
 if [ $out -gt $threshold ];
 then
-echo 'disk usage is high!'
-pid=$(ps aux | grep $program | awk '{print $2}' | head -1)
-echo $pid
+echo 'disk usage is high!' >> $logfile
+pid=$(ps cax | grep $program | awk '{print $1}' | head -1)
+if [ $? -eq 0 ];
+then
+echo "Process $program with pid- $pid, is throttling the disk resource!" >> $logfile
 else
-echo 'disk usage is normal!'
+echo "Some process is throttling the disk resource!" >> $loggile
 fi
+else
+echo "disk usage is normal!" >> $logfile
+fi
+
+echo >> $logfile
