f878045e8bf093aa5ef39f2c359281c85ec32bb3
yogeshagr <yogeshiiith@gmail.com>
2018-06-28 09:51:05 +0100

    validate file path

--- linux/bash-scripts/log-processing.sh
+++ linux/bash-scripts/log-processing.sh
@@ -8,12 +8,29 @@ function usage() {
   echo "To get usage: ./tunnel.sh usage"
 }
 
+function process_file() {
+file_path="$1"
+   while read -r line; do
+   echo "$line";
+ done < "$file_path"
+}
+
+function check_file_exist() {
+  if [ -f "$1" ]; then
+    :
+  else
+    echo "File does not exist!"; exit 1
+  fi
+}
+
 function main() {
   if [ $# -eq 0 ]
   then
     usage
     exit 1
   fi
+  check_file_exist "$1"
+  process_file "$1"
 }
 
 main "$@"
